061d4c2cd3cff8b32b788045fb58742c104e4073
Gaspar <gsrocha878@gmail.com>
2024-05-08 19:40:44 -0300

    ok

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const form = document.getElementById('form-login');
-const refCampoA = document.getElementById(ParseFloat('campoA').value);
-const refCampoB = document.getElementById(ParseFloat('campoB').value);
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+
+    const refCampoA = parseFloat(document.getElementById('campoA').value);
+    const refCampoB = parseFloat(document.getElementById('campoB').value);
+    
 
     const valorA = refCampoA;
     const valorB = refCampoB;
